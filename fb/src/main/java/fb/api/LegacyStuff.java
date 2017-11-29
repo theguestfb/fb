c125528131901ce91204baa711cc30cdeb5d244b
fictionbranches <fictionbranches@outlook.com>
2017-11-29 13:20:25 -0500

    Redirect fblatest.pl to /fb/recent

--- fb/src/main/java/fb/api/LegacyStuff.java
+++ fb/src/main/java/fb/api/LegacyStuff.java
@@ -1,13 +1,16 @@
 package fb.api;
 
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import fb.Strings;
 import fb.db.DB;
 import fb.db.DB.DBException;
 @Path("")
@@ -22,12 +25,12 @@ public class LegacyStuff {
 	@GET
 	@Path("legacy/{oldId}")
 	@Produces(MediaType.TEXT_HTML + "; charset=UTF-8")
-	public Response legacy(@PathParam("oldId") String oldId) {
+	public Response legacy(@PathParam("oldId") String oldId, @CookieParam("fbtoken") Cookie fbtoken) {
 		String newId;
 		try {
 			newId = DB.getLegacyId(oldId);
 		} catch (DBException e) {
-			return Response.ok("Not found: " + oldId).build();
+			return Response.ok(Strings.getFile("generic.html", fbtoken).replace("$EXTRA", "Not found: " + oldId)).build();
 		}
 		System.out.println("0Redirecting to" + GetStuff.createURI("/fb"));
 		return Response.seeOther(GetStuff.createURI("/fb/get/" + newId)).build();
@@ -36,67 +39,74 @@ public class LegacyStuff {
 	@GET
 	@Path("legacy/the-forum/{oldId}")
 	@Produces(MediaType.TEXT_HTML + "; charset=UTF-8")
-	public Response legacy1(@PathParam("oldId") String oldId) {
+	public Response legacy1(@PathParam("oldId") String oldId, @CookieParam("fbtoken") Cookie fbtoken) {
 		if (oldId.trim().toLowerCase().compareTo("root") == 0) return Response.seeOther(GetStuff.createURI("/fb/get/1")).build();
-		return legacy(oldId);
+		return legacy(oldId, fbtoken);
 	}
 	
 	@GET 
 	@Path("legacy/you-are-what-you-wish/{oldId}")
 	@Produces(MediaType.TEXT_HTML + "; charset=UTF-8")
-	public Response legacy2(@PathParam("oldId") String oldId) {
+	public Response legacy2(@PathParam("oldId") String oldId, @CookieParam("fbtoken") Cookie fbtoken) {
 		if (oldId.trim().toLowerCase().compareTo("root") == 0) return Response.seeOther(GetStuff.createURI("/fb/get/2")).build();
-		return legacy(oldId);
+		return legacy(oldId, fbtoken);
 	}
 	
 	@GET
 	@Path("legacy/altered-fates/{oldId}")
 	@Produces(MediaType.TEXT_HTML + "; charset=UTF-8")
-	public Response legacy3(@PathParam("oldId") String oldId) {
+	public Response legacy3(@PathParam("oldId") String oldId, @CookieParam("fbtoken") Cookie fbtoken) {
 		if (oldId.trim().toLowerCase().compareTo("root") == 0) return Response.seeOther(GetStuff.createURI("/fb/get/3")).build();
-		return legacy(oldId);
+		return legacy(oldId, fbtoken);
 	}
 	
 	@GET
 	@Path("legacy/the-future-of-gaming/{oldId}")
 	@Produces(MediaType.TEXT_HTML + "; charset=UTF-8")
-	public Response legacy4(@PathParam("oldId") String oldId) {
+	public Response legacy4(@PathParam("oldId") String oldId, @CookieParam("fbtoken") Cookie fbtoken) {
 		if (oldId.trim().toLowerCase().compareTo("root") == 0) return Response.seeOther(GetStuff.createURI("/fb/get/4")).build();
-		return legacy(oldId);
+		return legacy(oldId, fbtoken);
 	}
 	
 	@GET
 	@Path("legacy/cgi-bin/fbstorypage.pl")
 	@Produces(MediaType.TEXT_HTML + "; charset=UTF-8")
-	public Response legacy5(@QueryParam("page") String oldId) {
-		return legacy(oldId);
+	public Response legacy5(@QueryParam("page") String oldId, @CookieParam("fbtoken") Cookie fbtoken) {
+		return legacy(oldId, fbtoken);
+	}
+	
+	@GET
+	@Path("legacy/cgi-bin/fblatest.pl")
+	@Produces(MediaType.TEXT_HTML + "; charset=UTF-8")
+	public Response legacyRecent() {
+		return Response.seeOther(GetStuff.createURI("/fb/recent")).build();
 	}
 	
 	@GET
 	@Path("legacy/{anything}/{oldId}")
 	@Produces(MediaType.TEXT_HTML + "; charset=UTF-8")
-	public Response legacyCatchAll(@PathParam("oldId") String oldId, @PathParam("anything") String anything) {
+	public Response legacyCatchAll(@PathParam("oldId") String oldId, @PathParam("anything") String anything, @CookieParam("fbtoken") Cookie fbtoken) {
 		System.out.println("1Redirecting to" + GetStuff.createURI("/fb"));
 		if (oldId.trim().toLowerCase().compareTo("root") == 0) return Response.seeOther(GetStuff.createURI("/fb")).build();
-		return legacy(oldId);
+		return legacy(oldId, fbtoken);
 	}
 	
 	@GET
 	@Path("legacy/{anything}/{anything2}/{oldId}")
 	@Produces(MediaType.TEXT_HTML + "; charset=UTF-8")
-	public Response legacyCatchAll(@PathParam("oldId") String oldId, @PathParam("anything") String anything, @PathParam("anything2") String anything2) {
+	public Response legacyCatchAll(@PathParam("oldId") String oldId, @CookieParam("fbtoken") Cookie fbtoken, @PathParam("anything") String anything, @PathParam("anything2") String anything2) {
 		System.out.println("2Redirecting to" + GetStuff.createURI("/fb"));
 		if (oldId.trim().toLowerCase().compareTo("root") == 0) return Response.seeOther(GetStuff.createURI("/fb")).build();
-		return legacy(oldId);
+		return legacy(oldId, fbtoken);
 	}
 	
 	@GET
 	@Path("legacy/{anything}/{anything2}/{anything3}/{oldId}")
 	@Produces(MediaType.TEXT_HTML + "; charset=UTF-8")
-	public Response legacyCatchAll(@PathParam("oldId") String oldId, @PathParam("anything") String anything, @PathParam("anything3") String anything3, @PathParam("anything2") String anything2) {
+	public Response legacyCatchAll(@PathParam("oldId") String oldId, @CookieParam("fbtoken") Cookie fbtoken, @PathParam("anything") String anything, @PathParam("anything3") String anything3, @PathParam("anything2") String anything2) {
 		System.out.println("3Redirecting to" + GetStuff.createURI("/fb"));
 		if (oldId.trim().toLowerCase().compareTo("root") == 0) return Response.seeOther(GetStuff.createURI("/fb")).build();
-		return legacy(oldId);
+		return legacy(oldId, fbtoken);
 	}
 	
 }
