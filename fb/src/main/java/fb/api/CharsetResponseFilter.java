c7a34a9a0264d1ddee2df19f9f8830d76c2e4c09
fictionbranches <fictionbranches@outlook.com>
2017-12-03 18:39:55 -0500

    Updates

---
+++ fb/src/main/java/fb/api/CharsetResponseFilter.java
@@ -0,0 +1,24 @@
+package fb.api;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+
+import javax.ws.rs.core.MediaType;
+
+public class CharsetResponseFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext request, ContainerResponseContext response) {
+        MediaType type = response.getMediaType();
+        if (type != null) {
+            String contentType = type.toString();
+            if (!contentType.contains("charset")) {
+                contentType = contentType + ";charset=utf-8";
+                response.getHeaders().putSingle("Content-Type", contentType);
+            }
+        }
+  
+    }
+}
+
+
